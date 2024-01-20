e414dffd58862768b9bb59b06269b049380ab1c6
ahmed.merza <ahmed.merza@xs4arabia.com>
2024-01-20 14:41:30 +0300

    changed AppServiceProvider to AramexServiceProvider

--- config/app.php
+++ config/app.php
@@ -163,7 +163,7 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
+        App\Providers\AramexServiceProvider::class,
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
