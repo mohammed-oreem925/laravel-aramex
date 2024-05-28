0574e9588b70771bb857f48bfc040c41fc5f3e0e
Ahmed Hasan <67040497+AhmedMerza@users.noreply.github.com>
2024-05-28 07:51:29 +0000

    add to aramex_crednetials table test_username, test_password, and add delivery_service_id column to allow users to have multiple aramex accounts linked with their user

---
+++ database/migrations/2024_05_28_094741_add_test_username,_test_password,_delivery_service_id_to_aramex_crednetials_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('aramex_credentials', function (Blueprint $table) {
+            $table->string('test_username')->nullable();
+            $table->string('test_password')->nullable();
+            $table->renameColumn('username', 'live_username');
+            $table->renameColumn('password', 'live_password');
+            $table->unsignedBigInteger('delivery_service_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('aramex_credentials', function (Blueprint $table) {
+            $table->dropColumn('test_username');
+            $table->dropColumn('test_password');
+            $table->renameColumn('live_username', 'username');
+            $table->renameColumn('live_password', 'password');
+            $table->dropColumn('delivery_service_id');
+        });
+    }
+};
